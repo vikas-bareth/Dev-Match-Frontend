51f354bd413fc9ab3fecb44c948eba9a4096e30d
Vikas <iamvikasbareth@gmail.com>
2025-04-20 17:53:31 +0530

    urls updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,11 @@
 // export const APP_BASE_URL = "http://localhost:7777";
 export const APP_BASE_URL = "/api";
-export const LOGIN = "/auth/login";
-export const LOGOUT = "/auth/logout";
-export const GET_USER = "/auth/user";
-export const UPDATE_USER = "/auth/user";
-export const SIGNUP = "/auth/signup";
+export const LOGIN = "auth/login";
+export const LOGOUT = "auth/logout";
+export const GET_USER = "auth/user";
+export const UPDATE_USER = "auth/user";
+export const SIGNUP = "auth/signup";
 
-export const GET_FEED = "/user/feed";
-export const GET_CONNECTIONS = "/user/connections";
-export const GET_CONNECTION_REQUESTS = "/user/requests";
+export const GET_FEED = "user/feed";
+export const GET_CONNECTIONS = "user/connections";
+export const GET_CONNECTION_REQUESTS = "user/requests";
